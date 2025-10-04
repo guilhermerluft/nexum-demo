f0edef412c7a3f65e14684cf1bb72f9ac809aa39
Guilherme Luft <guilherme.luft@cwi.com.br>
2025-10-04 12:54:06 -0300

    feat: estrutura API

---
+++ api/nexum-api/src/main/java/br/com/nexum/nexum_api/NexumApiApplication.java
@@ -0,0 +1,13 @@
+package br.com.nexum.nexum_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NexumApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NexumApiApplication.class, args);
+	}
+
+}
---
+++ api/nexum-api/src/test/java/br/com/nexum/nexum_api/NexumApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.nexum.nexum_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NexumApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
